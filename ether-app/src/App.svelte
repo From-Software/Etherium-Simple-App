994b7b35b8977676a4fdad3cdc4e8606489f0698
Itahand Naizir <itahand.naizir@gmail.com>
2022-03-14 15:52:01 -0500

    Deployed test Smart Contract.

--- ether-app/src/App.svelte
+++ ether-app/src/App.svelte
@@ -1,10 +1,57 @@
 <script>
-	export let name;
+	import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+	import { ethers } from 'ethers';
+  const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  let greeting = '';
+	let actual;
+
+	async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      console.log({ provider })
+      const contract = new ethers.Contract(address, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+				actual = data;
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+	async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log({ provider })
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(address, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<div>
+		<header>
+			<button on:click={fetchGreeting}>Fetch Greeting</button>
+			<button on:click={setGreeting}>Set Greeting</button>
+			<input bind:value={greeting} placeholder="Set greeting">
+			<h1>{greeting}</h1>
+			<h1>The current phrase on the Blockchain is: "{actual}"</h1>
+		</header>
+	</div>
 </main>
 
 <style>
